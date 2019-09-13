c678336d6d7e1290217986319ae13b8ec8fa890f
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-09-13 10:00:31 -0400

    Declare number variables

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,11 +3,15 @@ public class MathTester {
     public static void main(String[] args){
         String firstWord;
         String secondWord;
+        int firstNumber,secondNumber;
+
+
 
 
         firstWord = "dog";
         secondWord = "house";
-        
+
+        System.out.println(firstWord+secondWord);
 
 
 
