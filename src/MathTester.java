f1199fb5cdfbb6dc8a165a9c0d5fd1216c61f871
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-09-16 09:48:56 -0400

    Initialized number variables and did operations

--- src/MathTester.java
+++ src/MathTester.java
@@ -10,8 +10,17 @@ public class MathTester {
 
         firstWord = "dog";
         secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
 
         System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+        System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+        System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
+        System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
+        System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("The modulus of the secondNumber by the firstNumber" +
+                " is "+ (secondNumber%firstNumber));
 
 
 
